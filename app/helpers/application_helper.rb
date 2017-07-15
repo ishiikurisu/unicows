36d6f1ed2ece3ff04b2b9c78e11a45357f1867e5
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-15 15:09:54 -0300

    Parsing HTML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,32 @@
 require "open-uri"
 
 module ApplicationHelper
+    # Loads the HTML content from a valid URL.
     public
     def ApplicationHelper.load url
         open(url) { |f| f.read }
+    end
+
+    # Extracts the HTML inside the given tag with parameters from a HTML string.
+    public
+    def ApplicationHelper.get_innerHTML html, tag
+        outlet = [ ]
+        offset = 0
+        opening_tag = "<#{tag}>"
+        closing_tag = "</#{tag.split[0]}>"
+
+        loop do
+            from = html.index(tag, offset)
+            unless from.nil?
+                to = html.index(closing_tag, from)
+                from += tag.length+1
+                outlet << html[from...to]
+                offset = to
+            else
+                break
+            end
+        end
+
+        outlet
     end
 end
--- test/helpers/test_net.rb
+++ test/helpers/test_net.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require "./app/helpers/application_helper"
 
 class TestNet < MiniTest::Test
-    
+
     # Runs before each test
     def setup
         @testurl = "https://www.instagram.com/_boxofjoe_/"
@@ -12,5 +12,12 @@ class TestNet < MiniTest::Test
         contents = ApplicationHelper.load @testurl
         assert contents
         assert contents.include? '<script type="text/javascript">'
+    end
+
+    def test_can_parse_stuff_from_html
+        html = ApplicationHelper.load @testurl
+        innies = ApplicationHelper.get_innerHTML html, 'script type="text/javascript"'
+        puts innies
+        assert_equal 4, innies.length
     end
 end
