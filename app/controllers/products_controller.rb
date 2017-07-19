25159c57f9eb60e806bfdab1f531f3e6de6c1e6a
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-07-18 22:18:57 -0300

    Creating products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,30 @@
+require './app/helpers/application_helper'
+require './app/models/image'
+
+class ProductsController < ApplicationController
+    attr_reader :raw
+    attr_reader :products
+
+    # Lists all available products
+    def list
+        # TODO Make this line work before the constructor
+        @products = ProductsController.get_first_products
+    end
+
+    # Displays a specific product
+    def show
+        puts "REQUEST: #{request}"
+    end
+
+    # Downloads the first products on the shop.
+    def ProductsController.get_first_products
+        @raw = ApplicationHelper.parse_json(ApplicationHelper.load("https://instagram.com/unicowsstore/media"))
+        @products = [ ]
+        @raw['items'].each do |item|
+            image = Image.new item
+            @products << image
+        end
+        # TODO Download remaining products
+        return @products
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'main#index'
   get 'main/about', to: 'main#about'
+  get 'products/list', to: 'products#list'
+  get 'products/show', to: 'products#show'
 end
