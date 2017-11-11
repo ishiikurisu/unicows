846295bec3c50cd040c442e325cd4778dc6a7ec4
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-11-11 00:17:27 -0200

    Trying to download some required stuff

--- app/helpers/drive_helper.rb
+++ app/helpers/drive_helper.rb
@@ -1,7 +1,13 @@
 require 'google_drive'
+require 'pathname'
 
 module DriveHelper
     def DriveHelper.download_worksheet title
+        gd_json = Pathname.new './config/gd.json'
+        unless gd_json.exist?
+            p `curl -o ./config/gd.json #{ENV['GD_JSON']}`
+        end
+
         session = GoogleDrive::Session.from_service_account_key('./config/gd.json')
         spreadsheet = session.spreadsheet_by_title title
         worksheet = spreadsheet.worksheets.first
