c20148fe2061116d70c971fc2bac692f332ecd11
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-11-10 23:57:08 -0200

    Fixing about page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,9 +7,10 @@ class MainController < ApplicationController
     end
 
     def about
-        unless defined? $album
-            $album = Album.new
-        end
-        @images = $album.images.take 3
+        @images = [
+            'https://instagram.fbsb4-1.fna.fbcdn.net/t51.2885-15/e35/19984565_487796128235495_3360415122861326336_n.jpg',
+            'https://instagram.fbsb4-1.fna.fbcdn.net/t51.2885-15/s640x640/sh0.08/e35/19764860_683714328490093_3798162845414195200_n.jpg',
+            'https://instagram.fbsb4-1.fna.fbcdn.net/t51.2885-15/s640x640/sh0.08/e35/19761175_851600388321431_7635192864229556224_n.jpg'
+        ]
     end
 end
--- app/models/Album.rb
+++ app/models/Album.rb
@@ -12,17 +12,14 @@ class Album
         @tags = @worksheet[0]
         @worksheet[1..-1].each_index do |line|
             row = @worksheet[line+1]
-            info = { 'id' => line+1 }
+            info = { 'id' => line }
             @tags.each_index do |index|
                 tag = @tags[index]
                 info[tag] = row[index]
             end
-            puts "---"
-            puts info
             image = Image.new info
             @images << image
         end
-        puts "...1"
 
         @pages = ApplicationHelper.each_slice @images.select { |e| e.is_on_sale? }, 6
     end
